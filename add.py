90c457a58f256231431bf9219dd577eff5aa9ae7
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2024-01-29 20:44:14 +0900

    Update add.py
    Edit version.

--- add.py
+++ add.py
@@ -1,1 +1,8 @@
 import sys
+
+iter = int(sys.stdin.readline())
+
+test = [list(map(int, sys.stdin.readline().split())) for i in range(iter)]
+
+for i in range(iter):
+  print(f"Case #{i+1}: {test[i][0]+test[i][1]}")
