9bc6194b2f0b82fd9d6cecfcfaef3efa251594f2
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2025-03-26 17:42:42 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1376 ms, Memory: 48036 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+cnt = [0] * 2000001
+
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    cnt[num] += 1
+
+for i in range(1000000, -1000001, -1):
+    if cnt[i] != 0:
+        for _ in range(cnt[i]):
+            print(i)
