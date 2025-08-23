01d9339501c76f8482d35c6d6065eb52081a9eb2
Mingyu Cheon <79088638+Martinj-31@users.noreply.github.com>
2025-08-23 23:37:28 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    sort(citations.begin(), citations.end());
+    for (int i = 0; i < citations.size(); i++) {
+        int h_index = citations.size() - i;
+        if (citations[i] >= h_index)
+            return h_index;
+    }
+    return answer;
+}
